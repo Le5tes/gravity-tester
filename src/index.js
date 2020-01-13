cf66134a5d12622933e3b3b9eac20793e470266c
Tim Williamson <timothy.williamson@santander.co.uk>
2020-01-13 14:10:11 +0000

    add counting number of nodes

--- src/index.js
+++ src/index.js
@@ -6,11 +6,14 @@ runTest = async (numberOfBodies) => {
     treeBuilder = await TreeBuilder.create();
     bodies = generateRandomBodies(numberOfBodies, 1000, 100, 500)
     time1 = performance.now();
-    treeBuilder.buildToArray(bodies, 1000,0,0);
+   const buildArray =treeBuilder.buildToArray(bodies, 1000,0,0);
     time2 = performance.now();
     console.log(`Ran ${numberOfBodies} bodies in ${time2 - time1}ms`);
+    console.log(`created ${buildArray.length} nodes`)
 }
 
+runTest(5);
 runTest(1000);
 runTest(10000);
 runTest(20000);
+runTest(100000);
