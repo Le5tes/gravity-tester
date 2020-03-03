aaaad326d7a92d8e55c5aa9a17596483e429c278
Tim Williamson <timothy.williamson@santander.co.uk>
2020-03-03 16:49:42 +0000

    feat(index.js) add tests for number of empty nodes, and include build method

--- src/index.js
+++ src/index.js
@@ -5,11 +5,17 @@ const performance = require('perf_hooks').performance;
 runTest = async (numberOfBodies) => {
     treeBuilder = await TreeBuilder.create();
     bodies = generateRandomBodies(numberOfBodies, 1000, 100, 500)
+    time0 = performance.now();
+    treeBuilder.buildToArray(bodies, 1000,0,0);
     time1 = performance.now();
-   const buildArray =treeBuilder.buildToArray(bodies, 1000,0,0);
+    const buildArray = treeBuilder.buildToArray(bodies, 1000,0,0);
     time2 = performance.now();
-    console.log(`Ran ${numberOfBodies} bodies in ${time2 - time1}ms`);
-    console.log(`created ${buildArray.length} nodes`)
+    treeBuilder.build(bodies,1000,0,0);
+    time3 = performance.now();
+    console.log(`Ran buildToArray with ${numberOfBodies} bodies in ${time2 - time1}ms`);
+    console.log(`created ${buildArray.length} nodes`);
+    console.log(`Ran build with ${numberOfBodies} bodies in ${time3 - time2}ms`);
+    console.log("\n")
 }
 
 runTestInternalPerformanceLogs = async (numberOfBodies) => {
@@ -31,6 +37,18 @@ runRepeatedTest = async(numberOfBodies, times) => {
     })
 }
 
+runTestNumberOfEmptyNodes = async (numberOfBodies) => {
+    treeBuilder = await TreeBuilder.create();
+    bodies = generateRandomBodies(numberOfBodies, 1000, 100, 500)
+    result = treeBuilder.buildToArray(bodies, 1000,0,0);
+    nodes = result.length;
+    emptyNodes = result.filter(val => {
+        return (val[7] == -1 && val[8] == -1 && val[9] == -1 && val[10] == -1 && val[11] == -1)
+    }).length;
+
+    console.log(`Number of empty nodes: ${emptyNodes}/${nodes} (built with ${numberOfBodies} bodies)`)
+}
+
 runTests = async () => {
     await runTest(5);
     await runTest(1000);
@@ -39,6 +57,7 @@ runTests = async () => {
     await runTest(100000);
 
     await runTestInternalPerformanceLogs(20000);
+    await runTestNumberOfEmptyNodes(20000);
 
     await runRepeatedTest(10000, 20);
 }
